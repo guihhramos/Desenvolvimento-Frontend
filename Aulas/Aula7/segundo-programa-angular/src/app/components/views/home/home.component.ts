32242ef48df5e917a8aaa42675d2c2ec96458dde
guihhramos <guiramos902@gmail.com>
2024-09-18 18:16:37 -0300

    Aula 07: Definindo rotas com Angular Routing

--- Aulas/Aula7/segundo-programa-angular/src/app/components/views/home/home.component.ts
+++ Aulas/Aula7/segundo-programa-angular/src/app/components/views/home/home.component.ts
@@ -1,4 +1,6 @@
+import { Produto } from '../../../../app/models/produto';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  public produtos: Array<Produto> = new Array<Produto>();
+  public produtos2: Produto[] = []; // outra forma de criar lista
 
+  constructor() {
+    this.produtos.push(new Produto('Bolu de morangu', 500000))
+    this.produtos.push(new Produto('Bolu de chocolate', 15000))
+    this.produtos.push(new Produto('Bolu de milho', 25065))
+    this.produtos.push(new Produto('Bolu de baunilha', 2010))
+
+    console.log(this.produtos)
+  }
 }
